4787ee6fbddb0631f28f4a6c570a44b6df36fe16
Balazs Nadler <balazsnad@gmail.com>
2018-08-03 17:43:45 +0200

    Largest prime factor
    The prime factors of 13195 are 5, 7, 13 and 29.
    What is the largest prime factor of the number 600851475143 ?

--- Problem03.js
+++ Problem03.js
@@ -1,24 +1,33 @@
 //Project Euler - Problem 3 - Largest prime factor
-//(not an elegant solution)
-
 function largestPrimeFactor(number) {
   const maxPrimeNum = 15000;
-  let arr = Array.from(new Array(maxPrimeNum),(val,index)=>index+2);
-  let filtered = arr.filter((a) => a%2 != 0 && a%3 != 0 && a%5 != 0 && a%7 != 0);
-  let firstPrimes = [2,3,5,7];
-  let primeArr = firstPrimes.concat(filtered);
-  let i = 0;
+  const primes = [];
+  let arr = Array.from(new Array(maxPrimeNum),(val)=>1);
+  arr[0] = 0;
+  arr[1] = 0;
+  for(let i = 2; i <= Math.floor(Math.sqrt(maxPrimeNum)); i++){
+    if(arr[i] == 1){
+      for(let j = i * i; j < maxPrimeNum; j+=i){
+            arr[j] = 0;
+      }
+    }
+  }
+  for(let k = 0; k < arr.length; k++ ){
+    if(arr[k] == 1){
+      primes.push(k);  
+    }
+  }
+
+  let l = 0;
   let maxPrime = [];
   while (number != 1){
-    if(number % primeArr[i] == 0){
-      number /= primeArr[i];
-      maxPrime.push(primeArr[i]);
-      i = 0;      
+    if(number % primes[l] == 0){
+      number /= primes[l];
+      maxPrime.push(primes[l]);
+      l = 0;      
     }else{
-      i++;
+      l++;
     }
   }
   return Math.max(...maxPrime);
 }
-
-largestPrimeFactor(13195);
